9f6bdb0bd11dade8b1cf16747df99bff8b811584
Leonardo Cordeiro <leonardocordeiro737@gmail.com>
2025-03-13 15:04:48 -0300

    calculadora funções construtoras

--- aula59-calculadora-FConstructor/assets/js/index.js
+++ aula59-calculadora-FConstructor/assets/js/index.js
@@ -7,30 +7,41 @@ function Calculadora() {
     this.capturaClique = () => {
         document.addEventListener('click', event => {
             const el = event.target;
-            if (el.classList.contains('btn-num')) this.addNUmeroDisplay(el);
+            if (el.classList.contains('btn-num')) addNUmeroDisplay(el);
 
-            if (el.classList.contains('btn-clear')) this.clearDisplay();
+            if (el.classList.contains('btn-clear')) clearDisplay();
 
-            if (el.classList.contains('btn-del')) this.apagaUm();
+            if (el.classList.contains('btn-del')) apagaUm();
 
-            if (el.classList.contains('btn-eq')) this.realizarConta();
+            if (el.classList.contains('btn-eq')) realizarConta();
+
+            this.display.focus();
         });
     };
 
     this.inicia = () => {
         this.capturaClique();
+        this.capturaEnter();
+        
     };
 
+    this.capturaEnter = () => {
+        this.display.addEventListener('keypress', e => {
+            if (e.keyCode !== 13) return;
+            realizarConta();
+        })
+    }
 
-    this.clearDisplay = () => {
+
+    const clearDisplay = () => {
         this.display.value = '';
     }
 
-    this.apagaUm = () => {
+    const apagaUm = () => {
         this.display.value = this.display.value.slice(0, -1)
     }
 
-    this.realizarConta = () => {
+    const realizarConta = () => {
         let conta = this.display.value;
 
         try {
@@ -49,7 +60,7 @@ function Calculadora() {
     }
 
 
-    this.addNUmeroDisplay = el => this.display.value += el.innerText;
+    addNUmeroDisplay = el => this.display.value += el.innerText;
 
 }
 
