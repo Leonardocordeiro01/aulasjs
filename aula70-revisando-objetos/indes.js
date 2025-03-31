53ded870b9b71af6ec8a581b4918e3c596611e6f
Leonardo Cordeiro <leonardocordeiro737@gmail.com>
2025-03-31 16:37:23 -0300

    aula sobre objetos

--- aula70-revisando-objetos/indes.js
+++ aula70-revisando-objetos/indes.js
@@ -21,3 +21,50 @@ pessoa1.getDataNascimento = function () {
 for (let chave in pessoa1) {
   console.log(`${pessoa1[chave]}`);
 }
+
+console.log(" ");
+console.log(" ");
+
+// factory functions / classes
+function criaPessoa(nome, sobrenome) {
+  return {
+    nome,
+    sobrenome,
+    get nomeCompleto() {
+      //get faz que eu consiga chamar a função sem os parenteses ()
+      return `${this.nome} ${this.sobrenome}`;
+    },
+  };
+}
+
+const p1 = criaPessoa("leonardo", "cordeiro");
+console.log(p1.nomeCompleto);
+
+// constructor functions / classes
+
+function Pessoa(nome, sobrenome) {
+  this.nome = nome;
+  this.sobrenome = sobrenome;
+}
+
+// {} <- this / cria um objeto vasio e atrela a palavra this a ele
+const p2 = new Pessoa("leonardo", "cordeiro");
+console.log(p2);
+
+const p3 = {}; // é basicamente isso que a palavra new faz
+p3.nome = "refael";
+
+//p1.nome = 'lucas' -> isso nao modificaria a constante
+// p1 = (ENDERECODEMEMORIA) -> 'VALOR'
+// p1.ENDERECOMEMORIA = {nome: 'outra coisa'};
+// p1 = (NOVOENDERECOMEMEORIA);
+
+// travando objetos
+function Pessoa2(nome, sobrenome) {
+  this.nome = nome;
+  this.sobrenome = sobrenome;
+
+  Object.freeze(this);
+}
+const p4 = new Pessoa2("leonardo", "cordeiro");
+p1.nome = "qualquer coisa"; //isso nao vai alterar ele por causa do Object.freeze
